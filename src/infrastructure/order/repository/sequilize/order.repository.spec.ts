0e359453d417f26eb7953991a057951228de3374
sousair <sousair2001@gmail.com>
2024-02-19 13:55:16 -0300

    test: find order repository method

--- src/infrastructure/order/repository/sequilize/order.repository.spec.ts
+++ src/infrastructure/order/repository/sequilize/order.repository.spec.ts
@@ -148,4 +148,34 @@ describe('Order repository test', () => {
       ],
     });
   });
+
+  it("should find an order", async () => {
+    const customerRepository = new CustomerRepository();
+    const customer = new Customer('123', 'Customer 1');
+    const address = new Address('Street 1', 1, 'Zipcode 1', 'City 1');
+    customer.changeAddress(address);
+    await customerRepository.create(customer);
+
+    const productRepository = new ProductRepository();
+    const product = new Product('123', 'Product 1', 10);
+    await productRepository.create(product);
+
+    const orderItem = new OrderItem('1', product.name, product.price, product.id, 2);
+
+    const order = new Order('123', '123', [orderItem]);
+
+    const orderRepository = new OrderRepository();
+    await orderRepository.create(order);
+
+
+    const orderFind = await orderRepository.find(order.id);
+
+    expect(orderFind).toStrictEqual(order);
+  });
+
+  it("should throw an error when order not found", async () => {
+    const orderRepository = new OrderRepository();
+
+    await expect(orderRepository.find("999")).rejects.toThrow('Order not found');
+  });
 });
