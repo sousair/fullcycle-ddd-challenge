8ceb775378b97b7f640e65ed3d174a9c7b6b3443
sousair <sousair2001@gmail.com>
2024-02-19 14:02:16 -0300

    test: findAll repository method

--- src/infrastructure/order/repository/sequilize/order.repository.spec.ts
+++ src/infrastructure/order/repository/sequilize/order.repository.spec.ts
@@ -178,4 +178,33 @@ describe('Order repository test', () => {
 
     await expect(orderRepository.find("999")).rejects.toThrow('Order not found');
   });
+
+  it("should find all orders", async () => {
+    const customerRepository = new CustomerRepository();
+    const customer = new Customer('123', 'Customer 1');
+    const address = new Address('Street 1', 1, 'Zipcode 1', 'City 1');
+    customer.changeAddress(address);
+    await customerRepository.create(customer);
+
+    const product1 = new Product('123', 'Product 1', 10);
+    const product2 = new Product('124', 'Product 2', 20);
+    
+    const productRepository = new ProductRepository();
+    await productRepository.create(product1);
+    await productRepository.create(product2);
+
+    const orderItem1 = new OrderItem('1', product1.name, product1.price, product1.id, 2);
+    const orderItem2 = new OrderItem('2', product2.name, product2.price, product2.id, 1);
+
+    const order1 = new Order('123', customer.id, [orderItem1]);
+    const order2 = new Order('124', customer.id, [orderItem2]);
+
+    const orderRepository = new OrderRepository();
+    await orderRepository.create(order1);
+    await orderRepository.create(order2);
+
+    const orders = await orderRepository.findAll();
+
+    expect(orders).toStrictEqual([order1, order2]);
+  })
 });
